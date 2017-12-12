c267995fe26dbd5de08247873d817d76e3146dbf
Mythbran <matthewdangelo@hotmail.com>
2017-12-11 21:38:28 -0500

    ANother push for member class
    yea.... didn't work the first time

--- SoftwareEngAssignment/src/main/java/software/assignment/business/Member.java
+++ SoftwareEngAssignment/src/main/java/software/assignment/business/Member.java
@@ -13,6 +13,8 @@ public class Member{
     //INSTANTIATING ALL VARIABLES 
     private int uid = 0; 
     
+    private String admin = "no";
+    
     private boolean administrator = false;
     
     @NotNull (message = "Missing First Name")
@@ -39,8 +41,11 @@ public class Member{
     @Pattern(regexp = "([a-zA-Z'.,-]+\\s*)+", message = "Invalid password")
     private String password = "";  
     
-    private String admin = "no";
-
+    //GETTERS AND SETTERS FOR ALL VARIABLES 
+    
+    //ADMIN
+    //HAVE BOTH A STRING AND A BOOLEAN 
+    //FIGURE OUT WHICH ONE WORKS
     public String getAdmin() {
         return admin;
     }
@@ -48,7 +53,61 @@ public class Member{
     public void setAdmin(String admin) {
         this.admin = admin;
     }
+  
+    public boolean isAdministrator() {
+        return administrator;
+    }
 
+    public void setAdministrator(boolean administrator) {
+        this.administrator = administrator;
+    }
+    
+    //ACCOUNT ID 
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+    
+    //FIRST NAME 
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    //LAST NAME
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    //PHONE NUMBER
+    public String getpNumber() {
+        return pNumber;
+    }
+
+    public void setpNumber(String pNumber) {
+        this.pNumber = pNumber;
+    }
+
+    //CREDIT CARD
+    public String getcCard() {
+        return cCard;
+    }
+
+    public void setcCard(String cCard) {
+        this.cCard = cCard;
+    }
+    
+    //USERNAME & PASSWORD
     public String getuName() {
         return uName;
     }
@@ -64,54 +123,4 @@ public class Member{
     public void setPassword(String password) {
         this.password = password;
     }
-    
-    //GETTERS AND SETTERS FOR ALL VARIABLES 
-    
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-    public boolean isAdministrator() {
-        return administrator;
-    }
-
-    public void setAdministrator(boolean administrator) {
-        this.administrator = administrator;
-    }
-    public String getfName() {
-        return fName;
-    }
-
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
-    }
-
-    public String getpNumber() {
-        return pNumber;
-    }
-
-    public void setpNumber(String pNumber) {
-        this.pNumber = pNumber;
-    }
-
-    public String getcCard() {
-        return cCard;
-    }
-
-    public void setcCard(String cCard) {
-        this.cCard = cCard;
-    }
-    
-    
 }
