db244e61bc635cbcd61fb050791aeb3e5a6f737c
Mythbran <matthewdangelo@hotmail.com>
2017-12-12 01:37:41 -0500

    Not working idk
    This is for testing purposes. Does not work at all

--- SoftwareEngAssignment/src/main/java/software/assignment/data/memberDbase.java
+++ SoftwareEngAssignment/src/main/java/software/assignment/data/memberDbase.java
@@ -2,42 +2,33 @@
 
 package software.assignment.data;
 
+import java.sql.DriverManager;
 import software.assignment.business.Member;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+
 
 
 public class memberDbase{
     
     public static void insert(Member member){
-        Connection pool = Connection.getInstance();
-        java.sql.Connection connection = pool.getConnection();
-        PreparedStatement ps = null;
-        
-        String INSERT = "INSERT INTO member"
-                + "(fName, lName, pNumber, cCard, uName, admin, password)"
-                + "VALUES (?, ?, ?, ?, ?, ?, ?";
+       
         try{
-            ps = connection.prepareStatement(INSERT);
-            ps.setString(1, member.getfName());
-            ps.setString(2, member.getlName());
-            ps.setString(3, member.getpNumber());
-            ps.setString(4, member.getcCard());
-            ps.setString(5, member.getuName());
-            String adminTemp = member.getAdmin();
-            if(adminTemp == "yes"){
-                ps.setInt(6, 1);
-            }
-            else{
-                ps.setInt(6, 0);
-            }
-            ps.setString(7, member.getPassword());
-        } catch(SQLException e){
-            System.err.println(e);
-        } finally{
-            DatabaseUtil.closePreparedStatement(ps);
-            pool.freeConnection(connection);
-        }
+        //Class.forName("com.mysql.jbdc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/softwareeng?zeroDateTimeBehavior=convertToNull", "root", "sesame");
+        
+        Statement st = con.createStatement();        
+        String INSERT = "INSERT INTO test"
+                + "(test)"
+                + "VALUES ()";
+        
+            st.executeUpdate(INSERT); 
+            System.out.println("fuck this shit");
+        }catch(SQLException e){
+            System.out.println("data not insertted sucalsikdfghjn");
+        } 
             
                 
     }
